7c8d92cdc398c41bcf25109800b3525ca47f4eea
Grigoriy Sribnyy <ctrhtnysq.afqk@gmail.com>
2024-06-04 22:36:43 +0700

    Сделал изменение профиля

--- front/profileEdit/profileEdit.js
+++ front/profileEdit/profileEdit.js
@@ -1,52 +1,50 @@
 var token = localStorage.getItem('token');
 
 async function profilePut(data) {
-  const url = `'https://localhost:44305/api/User/profile`;
-
+  const url = 'https://localhost:44305/api/User/profile';
   const formData = new FormData();
   formData.append('Login', data.login);
   formData.append('BirthDate', data.date);
   formData.append('Avatar', data.file);
-
-  return fetch(url, {
+  
+  const requestOptions = {
     method: 'PUT',
+    headers: {
+      'accept': '*/*',
+      "Authorization": `Bearer ${token}`
+    },
     body: formData
-  })
-    .then(response => response.json())
+  };
+
+  return fetch(url, requestOptions)
     .then(result => {
       console.log(result);
       const errorMessage = document.getElementById('errorMessage');
       errorMessage.textContent = '';
-
-       if (result.error) {
-         errorMessage.textContent = result.error;
-         console.log(result.error);
-       }
-       
-      if (result.message) {
-        errorMessage.textContent = result.message;
-        console.log(result.message);
+      
+      if (result.error) {
+        errorMessage.textContent = result.error;
+        console.log(result.error);
       }
-
-       if (result.errors) {
+      
+      if (result.status == "200") {
+        errorMessage.textContent = result.message;
+        window.location.href = '../profile/profile.html';
+      }
+      
+      if (result.errors) {
         for (let key in result.errors) {
-            if (result.errors.hasOwnProperty(key)) {
-                errorMessage.textContent += `${key}: ${result.errors[key].join(' ')} \n`;
-                console.log(`${key}: ${result.errors[key].join(' ')}`);
-            }
+          if (result.errors.hasOwnProperty(key)) {
+            errorMessage.textContent += `${key}: ${result.errors[key].join(' ')} \n`;
+            console.log(`${key}: ${result.errors[key].join(' ')}`);
+          }
         }
-    }
-    
-      if (result.token) {
-        token = result.token;
-        localStorage.setItem('token', token);
-        window.location.href = '../mainWithAutorization/main.html';
       }
     })
     .catch(error => {
-      console.error('Ошибка', error);
+      console.error('Error', error);
       const errorMessage = document.getElementById('errorMessage');
-      errorMessage.textContent = 'Произошла ошибка при регистрации. Пожалуйста, попробуйте еще раз.';
+      errorMessage.textContent = 'Ошибка';
     });
 }
 
