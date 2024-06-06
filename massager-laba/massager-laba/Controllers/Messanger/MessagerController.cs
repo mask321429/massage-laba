8a848e52b2835443e645eb2c41b1e5ff0a7c6a6a
Maks <maxxx321429@mail.ru>
2024-06-06 19:42:16 +0700

    забыл про контроллер

--- massager-laba/massager-laba/Controllers/Messanger/MessagerController.cs
+++ massager-laba/massager-laba/Controllers/Messanger/MessagerController.cs
@@ -1,8 +1,32 @@
+using massager_laba.Interface;
 using Microsoft.AspNetCore.Mvc;
 
 namespace massager_laba.Controllers.Messanger;
 
 public class MessagerController : ControllerBase
 {
-    
+    private readonly IMeassagerService _meassagerService;
+
+    public MessagerController(IMeassagerService meassagerService)
+    {
+        _meassagerService = meassagerService;
+    }
+
+    public async Task<IActionResult> GetMessage()
+    {
+        try
+        {
+            var result = await _meassagerService.GetMyMessager(Guid.Parse(User.Identity.Name));
+            return Ok(result);
+        }
+        catch (KeyNotFoundException e)
+        {
+            return StatusCode(404, new { Error = e.Message });
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
 }
