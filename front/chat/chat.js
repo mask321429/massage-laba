251c4167eff904027c0dcc206344e0e869b4f226
Grigoriy Sribnyy <ctrhtnysq.afqk@gmail.com>
2024-06-15 14:39:45 +0700

    Выводится история. Осталось сделать получеение истории при прокрутке

--- front/chat/chat.js
+++ front/chat/chat.js
@@ -1,43 +1,41 @@
 var token = localStorage.getItem('token');
 const urlParams = new URLSearchParams(window.location.search);
+console.log(token);
 
-const from = urlParams.get('From');
+let myId;
 const to = urlParams.get('To');
-const name = urlParams.get('Name');
-
-console.log('From:', from);
 console.log('To:', to);
-console.log('Name:', name);
 
-document.getElementById('chatName').textContent = name;
+const url = `http://localhost:5294/api/Messager/history?idToUser=${to}&count=50`;
+getHistory(url, token);
 
-connectWebSocket();
 const messagesContainer = document.getElementById('web-socket');
 
-function connectWebSocket() {
+async function connectWebSocket() {
 
-    socket = new WebSocket(`ws://localhost:5294/ws?userId=${from}`);
+    socket = new WebSocket(`ws://localhost:5294/ws?userId=${myId}`);
+    console.log(myId);
 
-    socket.onopen = function() {
+    socket.onopen = function () {
         console.log('WebSocket connection established');
     };
 
-    socket.onerror = function(error) {
+    socket.onerror = function (error) {
         console.log('WebSocket error: ' + JSON.stringify(error));
     };
 
-    socket.onmessage = function(event) {
+    socket.onmessage = function (event) {
         const data = JSON.parse(event.data);
         displayMessage(data.Content, "message-other");
     };
 
-    socket.onclose = function(event) {
+    socket.onclose = function (event) {
         console.log(`WebSocket connection closed: ${event.code} - ${event.reason}`);
     };
 }
 
 
-function sendMessage() {
+async function sendMessage() {
     const messageInput = document.getElementById('messageInput');
     const message = messageInput.value;
 
@@ -45,7 +43,7 @@ function sendMessage() {
 
     if (socket && socket.readyState === WebSocket.OPEN) {
         socket.send(JSON.stringify({
-            FromUserId: from,
+            FromUserId: myId,
             ToUserId: to,
             Content: message
         }));
@@ -55,7 +53,7 @@ function sendMessage() {
     }
 }
 
-function displayMessage(message, user) {
+async function displayMessage(message, user) {
     const messageElement = document.createElement('div');
     messageElement.textContent = message;
     messageElement.classList.add(user);
@@ -63,3 +61,41 @@ function displayMessage(message, user) {
     const container = document.querySelector('.chatfield');
     container.scrollTop = container.scrollHeight;
 }
+
+async function getHistory(url, token) {
+    return fetch(url, {
+        method: 'GET',
+        headers: new Headers({
+            "Authorization": `Bearer ${token}`
+        }),
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(url);
+            console.log(data);
+            document.getElementById('chatName').textContent = data[0].name;
+            myId = data[0].myId;
+            console.log(myId);
+            console.log(data[0].name);
+
+            data[0].messages.forEach(function (element) {
+                if (element.whoseMessage == myId) {
+                    displayMessage(element.text, "message-my");
+                } else {
+                    displayMessage(element.text, "message-other");
+                }
+            });
+            connectWebSocket();
+
+        })
+        .catch(error => {
+            console.error('Ошибка', error);
+        });
+}
+
+const chatfield = document.querySelector('.chatfield');
+chatfield.addEventListener('scroll', function() {
+    if (chatfield.scrollTop === 0) {
+        console.log('Пользователь прокрутил содержимое .chatfield вверх');
+    }
+});
